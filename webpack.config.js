f9e71d8e3681373f999af66b19a36715ec5957bc
Nik Luparev <nikita.luparev@gmail.com>
2022-03-01 00:18:46 +0500

    remove /dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,6 +10,12 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
 
   module: {
     rules: [
