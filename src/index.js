49c4bd4437b176b98d879885a40959d9ffd80cad
Nik Luparev <nikita.luparev@gmail.com>
2022-02-28 23:42:45 +0500

    adjust

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from "lodash";
 
+import "./style.css";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack", "awesomer"], " ");
+  element.classList.add("hello");
+
+  element.appendChild(myIcon);
 
   return element;
 }
